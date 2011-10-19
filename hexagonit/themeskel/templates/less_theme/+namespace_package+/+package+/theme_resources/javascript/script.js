35426270449a5c676458d86d2c1ec853e21abdbd
Vilmos Somogyi <vsomogyi@gmail.com>
2011-10-19 17:18:08 +0200

    mobile search and navigation improvments

--- hexagonit/themeskel/templates/less_theme/+namespace_package+/+package+/theme_resources/javascript/script.js
+++ hexagonit/themeskel/templates/less_theme/+namespace_package+/+package+/theme_resources/javascript/script.js
@@ -1,8 +1,17 @@
 $('document').ready(function () {
     $('#mobile-globalnav').bind('click', function() {
         $('#portal-globalnav').toggleClass('visible');
+        $('#portal-searchbox').removeClass('visible');
+    });
+    
+    $('#mobile-search').bind('click', function() {
+        $('#portal-searchbox').toggleClass('visible');
+        $('#portal-globalnav').removeClass('visible');
     });
 });
+
+
+
 
 /*
  * MBP - Mobile boilerplate helper functions
