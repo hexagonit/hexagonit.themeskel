850ba3566282299f67e3a6bbc641ae2619bbb3a6
Vilmos Somogyi <vsomogyi@gmail.com>
2013-02-07 11:51:34 +0100

    Mobile styling fixes

--- hexagonit/themeskel/docs/HISTORY.rst
+++ hexagonit/themeskel/docs/HISTORY.rst
@@ -4,6 +4,7 @@ Changelog
 2.1 (2013-02-XX)
 ------------------
 
+- Mobile styling fixes [vsomogyi]
 - Language chooser and quicklinks added [vsomogyi]
 - Retina image support added [vsomogyi]
 - Multi-theme support added [vsomogyi]
--- hexagonit/themeskel/templates/less_theme/+namespace_package+/+package+/theme_resources/default/javascript/script.js
+++ hexagonit/themeskel/templates/less_theme/+namespace_package+/+package+/theme_resources/default/javascript/script.js
@@ -1,13 +1,19 @@
 (function ($) {
     $(function () {
         $('#mobile-globalnav').bind('click', function() {
-            $('#portal-globalnav').toggleClass('visible');
+            var $menu = $('#portal-globalnav');
+            $menu.toggleClass('visible');
             $('#portal-searchbox').removeClass('visible');
+            // show quicklinks only when the menu is closed
+            $('#quicklinks').toggle(!$menu.hasClass('visible'));
         });
-    
+
         $('#mobile-search').bind('click', function() {
-            $('#portal-searchbox').toggleClass('visible');
+            var $search = $('#portal-searchbox');
+            $search.toggleClass('visible');
             $('#portal-globalnav').removeClass('visible');
+            // show quicklinks only when the search is closed
+            $('#quicklinks').toggle(!$search.hasClass('visible'));
         });
     
         $('#mobile-page-settings').bind('click', function() {
